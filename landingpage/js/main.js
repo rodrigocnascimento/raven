dab32d69e02bbd2ad542e9af6d5028890adf6227
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2017-08-02 22:40:32 -0300

    mobile responsiviness

--- landingpage/js/main.js
+++ landingpage/js/main.js
@@ -1,20 +1,2 @@
 ;$(document).ready(function(){
-  $('.badhon-tab li').on('click', function() {
-    var $this = $(this)
-
-    $('.badhon-tab').each(function() {
-      $children = $(this).children()
-      $children.find('a').css({
-        color: '#ffd3e2',
-        border: 'none',
-      })
-      $children.find('i').css({color: '#ffd3e2'})
-    })
-
-    $this.find('a').css({
-      color: '#d96f93',
-      borderRight: '10px solid #d96f93',
-    })
-    $this.find('i').css({color: '#d96f93'})
-  })
 })

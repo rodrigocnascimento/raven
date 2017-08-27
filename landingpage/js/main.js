faf239f705a16685ed84f6b693d821448d46b51b
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2017-08-26 22:08:22 -0300

    launch updates :)

--- landingpage/js/main.js
+++ landingpage/js/main.js
@@ -1,2 +1,26 @@
 ;$(document).ready(function(){
+  $(".send-form").on('click', function(e) {
+    e.preventDefault()
+    var userEmail = $("#userEmail").val()
+    var applicationId = Date.now()
+    var data = JSON.stringify({
+      applicationId: applicationId,
+      email: userEmail
+    })
+    var api = $.ajax({
+      url: "https://xv755mjkqe.execute-api.us-east-1.amazonaws.com/prod/applicationform",
+      method: "POST",
+      contentType: "application/json",
+      data: data
+    })
+    api.done(function(data) {
+      if (Object.getOwnPropertyNames(data).length === 0) {
+        $(".submit-area").hide('slow')
+        $(".submit-success").show('slow')
+      }
+    })
+    api.fail(function(e) {
+      console.error('err', e)
+    })
+  })
 })
